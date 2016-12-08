bba2824f7fe57f7ae630b901b1eb859db6aafd48
charliesmart <charles.smart@uconn.edu>
2016-12-08 16:35:19 -0500

    Actually write some tests woohoo

--- tests/tests.js
+++ tests/tests.js
@@ -1,7 +1,14 @@
-describe('animationTiming', {
-  it('should return "SECONDS" when called first without an argument.', {
+describe('animationTiming([type])', {
+  it('returns "SECONDS" when called first without an argument.', function() {
     expect(
       animationTiming();
     ).toEqual('SECONDS');
+  });
+
+  it('returns "FRAMES" when called without an argument after being called with argument FRAMES', function() {
+    animationTiming(FRAMES);
+    expext(
+      animationTiming();
+    ).toEqual('FRAMES');
   });
 });
