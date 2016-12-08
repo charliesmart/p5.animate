0c2d75b09a7bf2a1294ee8586e402d2bf6e2b843
charliesmart <charles.smart@uconn.edu>
2016-12-08 16:55:40 -0500

    Finish tests for animationTiming

--- tests/tests.js
+++ tests/tests.js
@@ -1,14 +1,23 @@
 describe('animationTiming([type])', function() {
   it('returns "SECONDS" when called first without an argument.', function() {
-    expect(
-      animationTiming();
-    ).toEqual('SECONDS');
+    var timing = animationTiming();
+    expect(timing).toEqual('SECONDS');
   });
 
   it('returns "FRAMES" when called without an argument after being called with argument FRAMES', function() {
     animationTiming(FRAMES);
-    expext(
-      animationTiming();
-    ).toEqual('FRAMES');
+    var timing = animationTiming();
+    expect(timing).toEqual('FRAMES');
   });
+
+  it('does not change timing when invalid argument is passed', function() {
+    animationTiming(SECONDS);
+    var timing = animationTiming();
+    expect(timing).toEqual('SECONDS');
+
+    // Set to invalid value
+    animationTiming(QWERTY);
+    var timing = animationTiming();
+    expect(timing).toEqual('SECONDS');
+  })
 });
